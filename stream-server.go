b470f97a33196d787828f10d2a80355de12bc738
Merlin <merlin.regalado@dtsmine.pe>
2023-08-09 15:44:02 -0500

    feat: response as json

--- stream-server.go
+++ stream-server.go
@@ -1,36 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
-	"strconv"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/stream", streamHandler)
 	fmt.Println("Servidor de streaming iniciado en http://localhost:8080/stream")
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Listen and serve ERR:", err)
+	}
+
 }
 
-func streamHandler(w http.ResponseWriter, r *http.Request) {
+func streamHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// encoder
+	encoder := json.NewEncoder(w)
 
 	// Envía datos en un bucle cada segundo
 	for {
-		// Cambia el mensaje y el evento como desees
-		message := "streaming: " + time.Now().Format("2006-01-02 15:04:05") + " - " + strconv.Itoa(rand.Intn(100)) + " t"
-		event := "actualizacion"
+		data := Data{
+			Message: "streaming!",
+			Time:    time.Now().Format(time.RFC3339),
+			Ton:     rand.Intn(100),
+		}
 
-		fmt.Fprintf(w, "event: %s\n", event)
-		fmt.Fprintf(w, "data: %s\n\n", message)
+		if err := encoder.Encode(data); err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			return
+		}
 
-		w.(http.Flusher).Flush()
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
 
 		time.Sleep(1 * time.Second)
 	}
+}
+
+type Data struct {
+	Message string `json:"message"`
+	Time    string `json:"time"`
+	Ton     int    `json:"ton"`
 }
